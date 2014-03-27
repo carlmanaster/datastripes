836d99980b4d38fb760f68a87dadae6bdad0ff0a
Carl Manaster <carlm@resmed.com>
2014-03-26 17:46:41 -0700

    applied underscore each to select.js

--- resource/js/select.js
+++ resource/js/select.js
@@ -12,35 +12,19 @@
   _.extend(Select.prototype, {
   
     clear: function() {
-      var i;
-      for (i = 0; i < this.dataset.length; i++) {
-        this.dataset[i].selected = false;
-      }
+      _.each(this.dataset, function(item) {item.selected = false});
     },
   
     byIndex: function (min, max) {
-      var i;
-      for (i = 0; i < this.dataset.length; i++) {
-        this.dataset[i].selected = min <= i && i <= max;
-      }
+      _.each(this.dataset, function(item, index) {item.selected = min <= index && index <= max});
     },
     
     byValue: function (column, min, max) {
-      var i
-      , value;
-      for (i = 0; i < this.dataset.length; i++) {
-        value = this.dataset[i].data[column];
-        this.dataset[i].selected = min <= value && value <= max;
-      }
+      _.each(this.dataset, function(item) {item.selected = min <= item.data[column] && item.data[column] <= max});
     },
     
     selectedByValue: function(column, min, max) {
-      var i
-      ,   value;
-      for (i = 0; i < this.dataset.length; i++) {
-        value = this.dataset[i].data[column];
-        this.dataset[i].selected = this.dataset[i].selected  && min <= value && value <= max;
-      }
+      _.each(this.dataset, function(item) {item.selected = item.selected  && min <= item.data[column] && item.data[column] <= max});
     }
 
   });
