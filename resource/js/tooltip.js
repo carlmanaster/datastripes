8a597a256151af1a399657e47e0db9b73ca77785
Carl Manaster <carlmanaster@CManaster-MBP.local>
2016-01-18 16:56:32 -0800

    refactored tooltips

---
+++ resource/js/tooltip.js
@@ -0,0 +1,41 @@
+(function (datastripes) {
+
+  // Export "package"
+  datastripes.Tooltip = Tooltip;
+
+  function Tooltip() {
+  }
+
+  // Methods
+  _.extend(Tooltip.prototype, {
+
+    create: function() {
+      return d3.select("body").append("div")
+                  .attr("class", "tooltip")
+                  .style("opacity", 0);
+    },
+  
+    show: function(html) {
+      tooltip.transition()
+          .duration(500)
+          .style("opacity", .9);
+	    tooltip.html(html)
+	      .style("left", (d3.event.pageX) + "px")
+	      .style("top", (d3.event.pageY + 10) + "px");
+    },
+
+    hide: function() {
+      tooltip.transition()
+          .duration(500)
+          .style("opacity", 0);
+    },
+
+    hideImmediately: function() {
+      tooltip.transition()
+          .duration(0)
+          .style("opacity", 0);
+    }
+
+  });
+
+}(window.datastripes));
